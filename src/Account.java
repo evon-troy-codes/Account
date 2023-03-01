ab70c277cf56f13f6c07923a2378cf842e4cdb5b
Evon Troy Alexander <117135045+evon-troy-codes@users.noreply.github.com>
2023-03-01 18:13:37 -0500

    Made amendments to comments.

--- src/Account.java
+++ src/Account.java
@@ -6,46 +6,46 @@
 
 public class Account {
 
-	// Instance variables
+    // Instance variables
 
-	private String PIN;
-	protected double balance;
+    private String PIN;
+    protected double balance;
 
-	// Conditional statement is used to error check - PIN can only be 4 digits.
-	public void setPIN(String PIN) {
-		if (PIN.length() == 4) {
-			this.PIN = PIN;
-		} else {
-			System.out.println("The pin number should be 4 digits.");
-		}
-	}
+    // Conditional statement is used to error check - PIN can only be 4 digits.
+    public void setPIN(String PIN) {
+        if (PIN.length() == 4) {
+            this.PIN = PIN;
+        } else {
+            System.out.println("The pin number should be 4 digits.");
+        }
+    }
 
-	public boolean checkPIN(String PIN) {
-		return this.PIN.equals(PIN);
-	}
+    public boolean checkPIN(String PIN) {
+        return this.PIN.equals(PIN);
+    }
 
-	// Deposit method that will be overridden in the checking and savings class
-	// files.
-	public void deposit(double amount) {
-		if (amount >= 0) {
-			this.balance += amount;
-			this.balance = Math.round(this.balance);
-		}
-	}
+    // Deposit method that will be overridden in the checking and savings class
+    // files.
+    public void deposit(double amount) {
+        if (amount >= 0) {
+            this.balance += amount;
+            this.balance = Math.round(this.balance);
+        }
+    }
 
-	// Withdrawal method that will be overridden in the checking and savings class
-	// files.
-	public void withdraw(double amount) {
-		if (amount >= 0) {
-			this.balance -= amount;
-			this.balance = Math.round(this.balance);
-		}
-	}
+    // Withdrawal method that will be overridden in the checking and savings class
+    // files.
+    public void withdraw(double amount) {
+        if (amount >= 0) {
+            this.balance -= amount;
+            this.balance = Math.round(this.balance);
+        }
+    }
 
-	public double getBalance(String PIN) {
-		if (this.PIN.equals(PIN)) {
-			return this.balance;
-		}
-		return -1;
-	}
+    public double getBalance(String PIN) {
+        if (this.PIN.equals(PIN)) {
+            return this.balance;
+        }
+        return -1;
+    }
 }
--- src/CheckingAccount.java
+++ src/CheckingAccount.java
@@ -1,61 +1,58 @@
 
 public class CheckingAccount extends Account {
+    /*
+     * The checking account has a monthly fee of $15 if the balance of checking
+     * account is lower than $1500
+     */
+    private final double MONTHLY_FEE = 15;
 
-	// Instance varaibles.
+    /*
+     * The checking account can go into negative and an overdraft fee of $10 will
+     * apply each time.
+     */
+    private final double OVERDRAFT_FEE = 10;
 
-	/*
-	 * The checking account has a monthly fee of $15 if the balance of checking
-	 * account is lower than $1500
-	 */
-	private final double MONTHLY_FEE = 15;
+    /* A user can never go into overdraft protection more than $250. */
+    private final double OVERDRAFT_LIMIT = 250;
 
-	/*
-	 * The checking account can go into negative and an overdraft fee of $10 will
-	 * apply each time.
-	 */
-	private final double OVERDRAFT_FEE = 10;
+    public CheckingAccount() {
+        super();
+    }
 
-	/* A user can never go into overdraft protection more than $250. */
-	private final double OVERDRAFT_LIMIT = 250;
+    @Override
+    public void deposit(double amount) {
+        super.deposit(amount);
+    }
 
-	public CheckingAccount() {
-		super();
-	}
+    @Override
+    public void withdraw(double amount) {
+        if (this.balance - amount >= -OVERDRAFT_LIMIT) {
+            super.withdraw(amount);
+            if (this.balance < 0) {
+                super.withdraw(OVERDRAFT_FEE);
+            }
+            this.balance = Math.round(this.balance);
+        }
+    }
 
-	@Override
-	public void deposit(double amount) {
-		super.deposit(amount);
-	}
-
-	@Override
-	public void withdraw(double amount) {
-		if (this.balance - amount >= -OVERDRAFT_LIMIT) {
-			super.withdraw(amount);
-			if (this.balance < 0) {
-				super.withdraw(OVERDRAFT_FEE);
-			}
-			this.balance = Math.round(this.balance);
-		}
-	}
-
-	@Override
+    @Override
 //If the PIN is valid the users balance will output.
-	public double getBalance(String PIN) {
-		if (checkPIN(PIN)) {
-			System.out.println("Current balance of your checking account : $ " + this.balance);
-		} else {
-			System.out.println("You need to enter the correct PIN number to check your balance.");
-		}
-		return balance;
-	}
+    public double getBalance(String PIN) {
+        if (checkPIN(PIN)) {
+            System.out.println("Current balance of your checking account : $ " + this.balance);
+        } else {
+            System.out.println("You need to enter the correct PIN number to check your balance.");
+        }
+        return balance;
+    }
 
-	// A for loop is used to asses the monthly maintenance fee.
-	public void monthElapsed(int months) {
-		for (int i = 0; i < months; i++) {
-			if (this.balance < 1500) {
-				withdraw(MONTHLY_FEE);
-			}
-			this.balance = Math.round(this.balance);
-		}
-	}
+    // A for loop is used to asses the monthly maintenance fee.
+    public void monthElapsed(int months) {
+        for (int i = 0; i < months; i++) {
+            if (this.balance < 1500) {
+                withdraw(MONTHLY_FEE);
+            }
+            this.balance = Math.round(this.balance);
+        }
+    }
 }
